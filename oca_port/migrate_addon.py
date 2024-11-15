83cc5052f6f659474dccb00c781ac30429bc3889
trisdoan <doanminhtri8183@gmail.com>
2024-11-15 15:11:42 +0700

    [IMP] introduce Squash Bot

--- oca_port/migrate_addon.py
+++ oca_port/migrate_addon.py
@@ -8,6 +8,8 @@ import pkg_resources
 
 import click
 
+from oca_port.squash_bot_commits import SquashBotCommits
+
 from .port_addon_pr import PortAddonPullRequest
 from .utils import git as g
 from .utils.misc import Output, bcolors as bc
@@ -161,6 +163,7 @@ class MigrateAddon(Output):
         # Check if the addon has commits that update neighboring addons to
         # make it work properly
         PortAddonPullRequest(self.app, push_branch=False).run()
+        SquashBotCommits(self.app).run()
         self._print_tips(adapted=adapted)
         return True, None
 
